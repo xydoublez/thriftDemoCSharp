c3f3534b5c1b1c0fbd038e5ba5171f65fffc11c9
ghostwinter <598850836@qq.com>
2019-04-08 11:30:02 +0800

    添加IDL接口定义文件

---
+++ thriftDemoApi/thrift/calculator.thrift
@@ -0,0 +1,13 @@
+namespace cpp calculator
+namespace java com.msunsoft.service
+namespace csharp msunsoft.service
+namespace php calculator
+namespace perl calculator
+
+
+service calculator {
+    i32 add(1: i32 arg1, 2: i32 arg2)
+    i32 subtract(1: i32 arg1, 2: i32 arg2)
+    i32 multiply(1: i32 arg1, 2: i32 arg2)
+    i32 division(1: i32 arg1, 2: i32 arg2)
+}

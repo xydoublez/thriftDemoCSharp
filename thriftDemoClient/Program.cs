92951d748327876465dad2ced24eb56afb64a5ad
ghostwinter <598850836@qq.com>
2019-04-08 22:37:12 +0800

    客户端以及服务端 以及调用JAVA服务端都成功

--- thriftDemoClient/Program.cs
+++ thriftDemoClient/Program.cs
@@ -12,15 +12,26 @@ namespace thriftDemoClient
    
         static void Main()
         {
+            /*
+             * thrift的使用中一般是一个Server对应一个Processor和一个Transport，如果有多个服务的话，那必须要启动多个Server，
+             * 占用多个端口，这种方式显然不是我们想要的，所以thrift为我们提供了复用端口的方式，
+             * 通过监听一个端口就可以提供多种服务，
+             * 这种方式需要用到两个类：TMultiplexedProcessor和TMultiplexedProtocol。
+             
+             */
             IPAddress ip = IPAddress.Parse("192.168.43.1");
+
             using (TcpClient tcpClient = new System.Net.Sockets.TcpClient())
             {
-                tcpClient.Connect(ip, 25000);
+                tcpClient.Connect(ip, 25001);
                 using (TTransport transport = new TSocket(tcpClient))
                 {
-
-                    TProtocol protocol = new TBinaryProtocol(transport);
-                    Client client = new Client(protocol);
+                    TTransport frameTransport = new TFramedTransport(transport);
+                    
+                    TProtocol protocol = new TCompactProtocol(frameTransport);
+                    //如果服务端使用TMultiplexedProcessor接收处理，客户端必须用TMultiplexedProtocol并且指定serviceName和服务端的一致
+                    TMultiplexedProtocol multiplexedProtocol = new TMultiplexedProtocol(protocol, "test-server-rpc$com.msunsoft.service.calculator$2.0");
+                    Client client = new Client(multiplexedProtocol);
                     //transport.Open();
                     Console.WriteLine(client.add(1, 2));
                 }
--- thriftDemoServer/Program.cs
+++ thriftDemoServer/Program.cs
@@ -14,11 +14,18 @@ namespace testThriftIFace
         static void Main()
         {
             //TServerSocket serverTransport = new TServerSocket(new System.Net.Sockets.TcpListener(IPAddress.Parse("192.168.43.1"), 25000));
-            TServerSocket serverTransport = new TServerSocket(25000, 0, true);
-            msunsoft.service.calculator.Processor processor = new msunsoft.service.calculator.Processor(new Server());
-            TServer server = new TSimpleServer(processor, serverTransport);
-            Console.WriteLine("Starting server on port 25000 ...");
+            TServerSocket serverTransport = new TServerSocket(25001, 0, false);
+            //异步IO，需要使用TFramedTransport，它将分块缓存读取
+            var tfactory = new TFramedTransport.Factory();
+            //使用高密度二进制协议
+            var pfactory = new TCompactProtocol.Factory();
+            TMultiplexedProcessor processor = new TMultiplexedProcessor();
+            msunsoft.service.calculator.Processor calcProcessor = new msunsoft.service.calculator.Processor(new Server());
+            processor.RegisterProcessor("test-server-rpc$com.msunsoft.service.calculator$2.0", calcProcessor);
+            TThreadedServer server = new TThreadedServer(processor, serverTransport,tfactory,pfactory);
+            Console.WriteLine("Starting server on port 25001 ...");
             server.Serve();
+            
 
         }
     }

321cfcf07a93ab7211a75d31520b181a3df2fc9f
ghostwinter <598850836@qq.com>
2019-04-08 16:40:55 +0800

    修正client 用IP连接server报错的问题，原因默认client用tcpv6连接的

--- thriftDemoClient/Program.cs
+++ thriftDemoClient/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
 using Thrift.Protocol;
 using Thrift.Transport;
 using static msunsoft.service.calculator;
@@ -10,11 +12,21 @@ namespace thriftDemoClient
    
         static void Main()
         {
-            TTransport transport = new TSocket("localhost", 25000);
-            TProtocol protocol = new TBinaryProtocol(transport);
-            Client client = new Client(protocol);
-            transport.Open();
-            Console.WriteLine(client.add(1, 2));
+            IPAddress ip = IPAddress.Parse("192.168.43.1");
+            using (TcpClient tcpClient = new System.Net.Sockets.TcpClient())
+            {
+                tcpClient.Connect(ip, 25000);
+                using (TTransport transport = new TSocket(tcpClient))
+                {
+
+                    TProtocol protocol = new TBinaryProtocol(transport);
+                    Client client = new Client(protocol);
+                    //transport.Open();
+                    Console.WriteLine(client.add(1, 2));
+                }
+            }
+
+           
             Console.ReadLine();
         }
     }
--- thriftDemoServer/Program.cs
+++ thriftDemoServer/Program.cs
@@ -1,5 +1,6 @@
 ﻿
 using System;
+using System.Net;
 using Thrift.Protocol;
 using Thrift.Server;
 using Thrift.Transport;
@@ -12,7 +13,8 @@ namespace testThriftIFace
    
         static void Main()
         {
-            TServerSocket serverTransport = new TServerSocket(25000, 0, false);
+            //TServerSocket serverTransport = new TServerSocket(new System.Net.Sockets.TcpListener(IPAddress.Parse("192.168.43.1"), 25000));
+            TServerSocket serverTransport = new TServerSocket(25000, 0, true);
             msunsoft.service.calculator.Processor processor = new msunsoft.service.calculator.Processor(new Server());
             TServer server = new TSimpleServer(processor, serverTransport);
             Console.WriteLine("Starting server on port 25000 ...");
